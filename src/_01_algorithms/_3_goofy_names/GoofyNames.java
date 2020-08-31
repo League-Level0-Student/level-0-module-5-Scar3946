18b82c79532454c373cf4f59a1142d38644984ba
root <root@8ce2e5f2d8d4>
2020-08-31 23:59:04 +0000

    goofyname

--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -8,7 +8,7 @@ import javax.swing.JOptionPane;
 
 public class GoofyNames {
 	public static void main(String[] args) {
-		String goofyName = "";
+	String goofyname = "";
 
 		// 1. Ask the user to enter their name
  String name = JOptionPane. showInputDialog("whats your name?");
@@ -17,20 +17,28 @@ public class GoofyNames {
 System.out.println(name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
+for(int i= 0; i < name.length(); i++) {
+	System.out.println(i);
+
 
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
+			String letter = name.substring(i, i+1);
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-
-			
+	if ( i % 2 == 0) { 
+		letter =letter.toUpperCase();
+	}
+	else {
+		letter = letter.toLowerCase();
+	}
+	
 			// 6. ADD this String  (containing 1 char) to the goofyName String
-
-		
+goofyname +=  letter; 
+}
 		// 7. Use pop-up to show user their Goofy name
-
+JOptionPane.showMessageDialog(null, goofyname);
 	}
 }
 
