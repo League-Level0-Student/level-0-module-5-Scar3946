fe544144487c5b283144d5cbec63c6c4821c5e4a
root <root@45a1e718dc34>
2020-09-28 23:57:29 +0000

    happypet

--- src/_03_method_writing/_1_obedient_robot/Obedientrobot.java
+++ src/_03_method_writing/_1_obedient_robot/Obedientrobot.java
@@ -1,33 +1,40 @@
 package _03_method_writing._1_obedient_robot;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Obedientrobot {
 
 	
 	public static void main(String[] args) {
 		
 		
-		drawSquare();
+	
 			
-		Robot Nightstroke = new Robot () ;
 		
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		drawSquare();
 		
 	}
+		
+		
+		
+		
+	static Robot Nightstroke = new Robot () ;
+	
+		
+		
+		
+	
 
 	private static void drawSquare() {
-		// TODO Auto-generated method stub
+		for(int i = 0; i<3 ; i++) {
+	Nightstroke.penDown();
+	Nightstroke.move(70);
+	Nightstroke.turn(90);
+		}
+		Nightstroke.move(90);
 		
 	}
 }
--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -5,7 +5,7 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 
@@ -14,15 +14,45 @@ public class HappyPet {
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
-
+			int task = JOptionPane.showOptionDialog(null, "How do you want to make the pet happy?", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Cuddle", "Food", "water" }, null);
+			
+			System.out.println(task);
+		    if(task == 0) {
+		    	cuddle();
+		    }
+		    else if (task == 1) {
+		    food();
+		    }
+		    else if (task == 2) {
+		    	water();
+		    }
+		    	
 			// 5. Use user input to call the appropriate method created in step 4.
 
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
 	}
+	
+	static String pet= JOptionPane.showInputDialog(null, "what pet do you want to buy?");
+	
+	private static void cuddle() {
+		if(pet.equalsIgnoreCase("cat")) {
+		JOptionPane.showMessageDialog(null, "purr");
+		}
+	}
+    private static void food() {
+		if(pet.equalsIgnoreCase("cat")) {
+			
+		}
+	}
+    private static void water() {
+		if (pet.equalsIgnoreCase("cat")) {
+		JOptionPane.showMessageDialog(null, "lap");	
+		}
+	}
+
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
