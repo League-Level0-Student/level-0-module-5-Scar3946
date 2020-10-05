b67746a34a72a449cd9102d8c7adcc2f67a16709
root <root@1fbe6b60e839>
2020-10-05 23:59:14 +0000

    skillpractice

---
+++ src/_04_practice/_1_skill_practice/Skillpractice.java
@@ -0,0 +1,55 @@
+package _04_practice._1_skill_practice;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Skillpractice {
+
+
+public static void main(String[] args) {
+	
+String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+int cents = Integer.parseInt(dimes)* 10;
+JOptionPane.showMessageDialog(null, "you have " + cents + "cents");
+String height = JOptionPane.showInputDialog("how tall are you in inches?");
+int inches = Integer.parseInt(height);
+if( inches < 36)	{
+	JOptionPane.showMessageDialog(null, "Eat your wheaties");
+}
+ for(int i = 1; i<31; i++)	{
+	 
+	if(i%3 == 0 ){
+	 System.out.println(i);
+	 }
+ }
+	
+Random object = new Random();
+int randomnumber = object.nextInt(20);
+int printrandom = object.nextInt(10);
+System.out.println(randomnumber);	
+System.out.println(printrandom);
+
+JOptionPane.showMessageDialog(null, " the difference is " + (randomnumber-printrandom) );
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
